a796d4f6bb8b72246eb62a5e7aaa22357d28edbd
TantoFaz65517 <jaziel.campelo@escolar.ifrn.edu.br>
2025-09-29 17:59:45 +0000

    to atrasado hein pqp

---
+++ Listas/lista8/Paciente.py
@@ -0,0 +1,17 @@
+
+class Paciente:
+    def __init__(self, nome: str, cpf: str, telefone: str, nascimento: datetime):
+        self.__nome = nome 
+        self.__cpf = cpf
+        self.__telefone = telefone
+        self.__nascimento = nascimento
+
+    def Idade(self) -> int:
+        hoje = datetime.today()
+        if idade = (hoje.month, hoje.day) < (self.__nascimento.month, self.__nascimento.day):
+            idade -= 1
+        return idade
+        
+    def __str__(self) -> str
+        return (f"Paciente: {self.__nome}, CPF: {self.__cpf}, "
+            f"Telefone: {self.__telefone}, Idade: {self.idade()} anos")
